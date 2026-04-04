40b20c220bb7c0f52eb74aa4d60fe446bf112a5a
Meikkun <169344938+Meikkun@users.noreply.github.com>
2026-04-04 15:28:06 +0200

    Remove total timeout from live stream proxy

--- pkg/server/upstream_http.go
+++ pkg/server/upstream_http.go
@@ -10,3 +10,10 @@ const defaultUpstreamRequestTimeout = 30 * time.Second
 func newUpstreamHTTPClient() *http.Client {
 	return &http.Client{Timeout: defaultUpstreamRequestTimeout}
 }
+
+func newStreamingHTTPClient() *http.Client {
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.ResponseHeaderTimeout = defaultUpstreamRequestTimeout
+
+	return &http.Client{Transport: transport}
+}
